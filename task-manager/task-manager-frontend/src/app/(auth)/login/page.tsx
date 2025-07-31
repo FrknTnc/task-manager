6eb4bc2270851776522da5bc05dacc8154c8b39e
Furkan Ali Tunç <furkanalitunc42@gmail.com>
2025-08-01 02:24:34 +0300

    Update page.tsx

--- task-manager/task-manager-frontend/src/app/(auth)/login/page.tsx
+++ task-manager/task-manager-frontend/src/app/(auth)/login/page.tsx
@@ -86,7 +86,7 @@ export default function LoginPage() {
         </form>
 
         <p className="text-sm text-center text-gray-600">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <a href="/register" className="text-blue-600 hover:underline">
             Sign up here
           </a>
