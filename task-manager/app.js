1051f0a3090dedea8aa6f6a1f5715d154069a418
Furkan Ali Tunç <furkanalitunc42@gmail.com>
2025-08-01 11:45:32 +0300

    Update app.js

--- task-manager/app.js
+++ task-manager/app.js
@@ -33,7 +33,31 @@ io.on('connection', (socket) => {
 
 app.set('io', io); 
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://task-manager-three-gules.vercel.app' 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.warn('⛔ Blocked by CORS:', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.options('*', cors());
+app.use((req, res, next) => {
+  console.log(`🛬 ${req.method} ${req.originalUrl} | Origin: ${req.headers.origin}`);
+  next();
+});
+
 app.use(express.json());
 
 app.use((req, res, next) => {
